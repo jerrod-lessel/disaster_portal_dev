1a59704740eb7661af73bafc25bf28bc570c8690
Jerrod Lessel <jerrod.lessel@gmail.com>
2025-08-04 23:19:25 -0700

    Update map.js

--- map.js
+++ map.js
@@ -341,7 +341,7 @@ function getChargersInView() {
           const status = charger.StatusType?.Title ?? 'Unknown Status';
           const usage = charger.UsageType?.Title ?? 'Usage details not specified';
           const network = charger.OperatorInfo?.Title ?? 'Unknown Network';
-          const numPoints = charger.Connections.Quantity ?? 'N/A';
+          const numPoints = charger.Connections.ConnectionTypeID ?? 'N/A';
 
           // Equipment Details (we'll grab info from the first connection)
           let equipmentInfo = '<li>No equipment details</li>';
